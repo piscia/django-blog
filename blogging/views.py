5edd427410d922e4ae0144cb2327ef7d0f00e6df
piscia <jpiscia22@gmail.com>
2022-01-06 11:41:12 -0800

    BlogDetailView class functioning as expected

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from blogging.models import Post
 
@@ -25,11 +27,19 @@ def list_view(request):
     """i recieved an error when using render saying that 'list.html' template
     did not extis"""
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+# class BlogListView(ListView):
+#     model = Post
+#     template_name = 'blogging/list.html'
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
--- polling/urls.py
+++ polling/urls.py
@@ -1,9 +1,10 @@
 # polling/urls.py
 
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    #path('', list_view, name="poll_index"),
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>/', PollDetailView.as_view(), name="poll_detail"),
 ]
